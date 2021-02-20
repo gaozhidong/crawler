32fe189cd4585bd5e9af03e86141923f1c6fd200
gaozhidong <1011284295@qq.com>
2021-02-20 15:49:02 +0800

    基本算法

--- src/main/java/com/github/gaozhidong/Main.java
+++ src/main/java/com/github/gaozhidong/Main.java
@@ -6,19 +6,80 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        List<String> linkPool = new ArrayList<>();
+        Set<String> processedLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            String link = linkPool.remove(linkPool.size() - 1);
+
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+            if (isInterestingLink(link)) {
+                Document doc = httpgetPaseHtml(link);
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                storeIntoDatabaseItIsNewsPage(doc);
+                processedLinks.add(link);
+            }
+        }
+    }
+
+    private static void storeIntoDatabaseItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpgetPaseHtml(String link) {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("https://sina.cn");
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(link);
             System.out.println(response1.getStatusLine());
-            HttpEntity entity1;
-            entity1 = response1.getEntity();
-            System.out.println(EntityUtils.toString(entity1));
-        }
 
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link) &&
+                isNotLoginPage(link));
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
