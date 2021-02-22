777a78a852237a684b9d5f0aff2b2900129c9ad2
gaozhidong <1011284295@qq.com>
2021-02-22 15:17:57 +0800

    使用H2

--- src/main/java/com/github/gaozhidong/Main.java
+++ src/main/java/com/github/gaozhidong/Main.java
@@ -1,5 +1,6 @@
 package com.github.gaozhidong;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,33 +12,88 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
 
-        List<String> linkPool = new ArrayList<>();
-        Set<String> processedLinks = new HashSet<>();
-        linkPool.add("https://sina.cn");
+
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:/Volumes/projects/userProjects/crawler/news", USER_NAME, PASSWORD);
 
         while (true) {
+            // 待处理的链接
+            List<String> linkPool = loadUrlFromDatabase(connection, "SELECT * FROM LINKS_TO_BE_PROCESSED");
+
             if (linkPool.isEmpty()) {
                 break;
             }
             String link = linkPool.remove(linkPool.size() - 1);
-
-            if (processedLinks.contains(link)) {
+            insertLinkIntoDatabases(connection, link, "DELETE FROM LINKS_TO_BE_PROCESSED WHERE link = ?");
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
             if (isInterestingLink(link)) {
                 Document doc = httpgetPaseHtml(link);
-                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                parseUrlsFromPageAndStoreIntoDatabases(connection, doc);
                 storeIntoDatabaseItIsNewsPage(doc);
-                processedLinks.add(link);
+                insertLinkIntoDatabases(connection, link, "INSERT INTO LINKS_ALREADY_PROCESSED (link) VALUES  (?)");
             }
+        }
+    }
+
+    private static List<String> loadUrlFromDatabase(Connection connection, String sql) throws SQLException {
+        List<String> results = new ArrayList<>();
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return results;
+    }
+
+    private static void parseUrlsFromPageAndStoreIntoDatabases(Connection connection, Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            insertLinkIntoDatabases(connection, href, "INSERT INTO LINKS_TO_BE_PROCESSED (link) VALUES (?) ");
+        }
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("SELECT LINK FROM LINKS_ALREADY_PROCESSED WHENEVER link")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                return true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return false;
+    }
+
+    private static void insertLinkIntoDatabases(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
         }
     }
 
